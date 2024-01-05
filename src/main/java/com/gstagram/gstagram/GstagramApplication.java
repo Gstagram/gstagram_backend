6967f1ce940d037598e35678903a67c332538b4e
Ahn Jiwan <synoti21@naver.com>
2024-01-06 00:34:57 +0900

    :tada: feat: initial commit

---
+++ src/main/java/com/gstagram/gstagram/GstagramApplication.java
@@ -0,0 +1,13 @@
+package com.gstagram.gstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GstagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gstagram/gstagram/GstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gstagram.gstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
