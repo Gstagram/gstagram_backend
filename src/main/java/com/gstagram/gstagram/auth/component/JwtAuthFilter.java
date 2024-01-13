e42aa422f24e26ba1928c5aa687b9070d1313037
Ahn Jiwan <synoti21@naver.com>
2024-01-13 22:39:01 +0900

    :sparkles: feat: JwtAuthFilter 및 Provider 구현 (#9)

--- src/main/java/com/gstagram/gstagram/auth/component/JwtAuthFilter.java
+++ src/main/java/com/gstagram/gstagram/auth/component/JwtAuthFilter.java
@@ -1,2 +1,40 @@
-package com.gstagram.gstagram.auth.component;public class JwtAuthFilter {
+package com.gstagram.gstagram.auth.component;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+        try{
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }catch (RedisConnectionFailureException e){
+            SecurityContextHolder.clearContext();
+            throw new ServletException();
+        } catch (Exception e){
+            throw new ServletException();
+        }
+
+        // 다음 Filter 실행
+        filterChain.doFilter(request, response);
+    }
 }
--- src/main/java/com/gstagram/gstagram/auth/component/JwtTokenProvider.java
+++ src/main/java/com/gstagram/gstagram/auth/component/JwtTokenProvider.java
@@ -1,2 +1,97 @@
-package com.gstagram.gstagram.auth.component;public class JwtTokenProvider {
+package com.gstagram.gstagram.auth.component;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private final UserDetailsService userDetailsService;
+
+    //내부 암호화 키
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    //1주일
+    private final long refreshTokenValidTime = 7 * 24 * 60 * 60 * 1000L;
+
+    public String createAccessToken(String userPK) {
+        Claims claims = Jwts.claims().setSubject(userPK);
+        Date now = new Date();
+        //1시간
+        long accessTokenValidTime = 60 * 60 * 1000L;
+        return Jwts.builder()
+                .signWith(key)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + accessTokenValidTime))
+                .compact();
+    }
+
+    public String createRefreshToken(String userPK) {
+        Claims claims = Jwts.claims().setSubject(userPK);
+        Date now = new Date();
+        String refreshToken = Jwts.builder()
+                .signWith(key)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + refreshTokenValidTime))
+                .compact();
+
+        redisTemplate.opsForValue().set(
+                userPK,
+                refreshToken,
+                refreshTokenValidTime,
+                TimeUnit.SECONDS
+        );
+        return refreshToken;
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(String.valueOf(this.getUserPk(token)));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUserPk(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwtToken);
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.error("ExpiredJwtException", e);
+            return false;
+        } catch(JwtException e) {
+            log.error("JwtException", e);
+            return false;
+        } catch (Exception e) {
+            log.error("Exception", e);
+            return false;
+        }
+    }
 }
