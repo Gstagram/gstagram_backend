94de3b668d28de6418e47c54966a0e8d80508e5f
Ahn Jiwan <synoti21@naver.com>
2024-01-16 22:34:20 +0900

    :sparkles: feat: Test에 따른 LoginReqDto, SignUpDto, ResponseTokenDto 선언 (#17)

---
+++ src/main/java/com/gstagram/gstagram/auth/dto/reqeust/LoginReqDto.java
@@ -0,0 +1,17 @@
+package com.gstagram.gstagram.auth.dto.reqeust;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class LoginReqDto {
+    @NotNull(message = "이메일을 입력해주세요.")
+    private String email;
+
+    @NotNull(message = "비밀번호를 입력해주세요.")
+    private String password;
+}
---
+++ src/main/java/com/gstagram/gstagram/auth/dto/reqeust/SignUpDto.java
@@ -0,0 +1,29 @@
+package com.gstagram.gstagram.auth.dto.reqeust;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SignUpDto {
+    @NotNull(message = "이메일을 입력해주세요.")
+    private String email;
+
+    @NotNull(message = "비밀번호를 입력해주세요.")
+    private String username;
+
+    @NotNull(message = "비밀번호를 입력해주세요.")
+    private String password;
+
+    @NotNull(message = "별명을 입력해주세요.")
+    private String nickname;
+
+    @NotNull(message = "프로필 사진 URL을 입력해주세요.")
+    private String profileS3ImageUrl;
+
+    @NotNull(message = "유저 인증 여부를 입력해주세요.")
+    private boolean isUserAuthorized;
+}
