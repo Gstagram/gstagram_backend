60647c55c88d8768f8c9dcafba675fa6f32a4945
Ahn Jiwan <synoti21@naver.com>
2024-01-19 01:25:43 +0900

    :goal_net: feat: dto validation 추가 (#17)

--- src/main/java/com/gstagram/gstagram/auth/dto/reqeust/SignUpDto.java
+++ src/main/java/com/gstagram/gstagram/auth/dto/reqeust/SignUpDto.java
@@ -1,5 +1,6 @@
 package com.gstagram.gstagram.auth.dto.reqeust;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,21 +10,23 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 public class SignUpDto {
-    @NotNull(message = "이메일을 입력해주세요.")
+    @NotNull
+    @NotBlank(message = "이메일을 입력해주세요.")
     private String email;
 
-    @NotNull(message = "비밀번호를 입력해주세요.")
+    @NotNull
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String username;
 
     @NotNull(message = "비밀번호를 입력해주세요.")
     private String password;
 
-    @NotNull(message = "별명을 입력해주세요.")
     private String nickname;
 
-    @NotNull(message = "프로필 사진 URL을 입력해주세요.")
+    @NotNull
+    @NotBlank(message = "프로필 사진 URL을 입력해주세요.")
     private String profileS3ImageUrl;
 
     @NotNull(message = "유저 인증 여부를 입력해주세요.")
-    private boolean userAuthroized;
+    private boolean userAuthorized;
 }
--- src/main/java/com/gstagram/gstagram/user/presentation/AuthController.java
+++ src/main/java/com/gstagram/gstagram/user/presentation/AuthController.java
@@ -1,2 +1,36 @@
-package com.gstagram.gstagram.user.presentation;public class AuthController {
+package com.gstagram.gstagram.user.presentation;
+
+import com.gstagram.gstagram.auth.application.AuthService;
+import com.gstagram.gstagram.auth.application.AuthServiceImpl;
+import com.gstagram.gstagram.auth.dto.reqeust.LoginReqDto;
+import com.gstagram.gstagram.auth.dto.reqeust.SignUpDto;
+import com.gstagram.gstagram.auth.dto.response.ResponseTokenDto;
+import com.gstagram.gstagram.common.api.ApiResponse;
+import com.gstagram.gstagram.common.api.ResponseCode;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Auth", description = "인증 관련 API")
+@Slf4j
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthServiceImpl authService;
+
+    @PostMapping("/signup")
+    public ApiResponse<Boolean> signUp(@RequestBody @Valid SignUpDto signUpDto){
+        return ApiResponse.success(ResponseCode.USER_CREATE_SUCCESS, authService.signUp(signUpDto));
+    }
+
+    @PostMapping("/login")
+    public ApiResponse<ResponseTokenDto> login(@RequestBody @Valid LoginReqDto loginReqDto){
+        return ApiResponse.success(ResponseCode.USER_LOGIN_SUCCESS, authService.login(loginReqDto));
+    }
 }
