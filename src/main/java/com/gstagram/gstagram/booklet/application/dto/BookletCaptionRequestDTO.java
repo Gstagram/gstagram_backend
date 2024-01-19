0f68f7628be5687055d6bdd81ca3d8cb75ab9708
MJ <alswns2631@cau.ac.kr>
2024-01-19 14:00:54 +0900

    :bug: Fix: object mapper을 위한 기본생성자 (#16)

--- src/main/java/com/gstagram/gstagram/booklet/application/dto/BookletCaptionRequestDTO.java
+++ src/main/java/com/gstagram/gstagram/booklet/application/dto/BookletCaptionRequestDTO.java
@@ -3,6 +3,7 @@ package com.gstagram.gstagram.booklet.application.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * Controller에서 service 코드를 쓰기 위한 DTO
@@ -11,6 +12,7 @@ import lombok.Getter;
  */
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class BookletCaptionRequestDTO {
     /***
