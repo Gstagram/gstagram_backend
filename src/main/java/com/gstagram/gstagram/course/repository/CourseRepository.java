9d34e8c2e577d78a5def81caf33e708a7db17b8e
MJ <alswns2631@cau.ac.kr>
2024-01-26 16:28:12 +0900

    :sparkles: Feat: city, region, course 제목을 통한 course 검색(#20)

--- src/main/java/com/gstagram/gstagram/course/repository/CourseRepository.java
+++ src/main/java/com/gstagram/gstagram/course/repository/CourseRepository.java
@@ -1,14 +1,17 @@
 package com.gstagram.gstagram.course.repository;
 
 import com.gstagram.gstagram.course.domain.Course;
+import com.gstagram.gstagram.course.repository.dto.CourseSearchCond;
 import com.gstagram.gstagram.region.domain.Region;
+import org.hibernate.query.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course, Long>{
+public interface CourseRepository extends JpaRepository<Course, Long>, CourseQueryRepository{
 
 
 
--- src/main/java/com/gstagram/gstagram/course/repository/dto/CourseSearchCond.java
+++ src/main/java/com/gstagram/gstagram/course/repository/dto/CourseSearchCond.java
@@ -1,2 +1,24 @@
-package com.gstagram.gstagram.course.repository.dto;public class CourseSearchCond {
+package com.gstagram.gstagram.course.repository.dto;
+
+import com.gstagram.gstagram.city.domain.City;
+import com.gstagram.gstagram.region.domain.Region;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+@Getter
+@AllArgsConstructor
+@Builder
+public class CourseSearchCond {
+
+    List<City> cityList;
+    List<Region> regionList;
+
+    String courseTitle;
+
+
+    /**
+     * todo: 나중에 hashTag있으면 hashtag에 대한 내용도 추가해야 한다
+     * */
 }
